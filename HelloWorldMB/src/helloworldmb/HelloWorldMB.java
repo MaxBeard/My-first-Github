b3f6a04fbf5957e647d01e17ac95660ef77cba3b
MaxBeard <mbeard@sjs.org>
2019-11-19 14:26:16 -0600

    Update HelloWorldMB.java

--- HelloWorldMB/src/helloworldmb/HelloWorldMB.java
+++ HelloWorldMB/src/helloworldmb/HelloWorldMB.java
@@ -1,11 +1,9 @@
 public class HelloWorldMB {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        for(int i=1;i<11;i++){
-            System.out.println(i);
+        //LMAO
         }
                
     }
     
-}
+
