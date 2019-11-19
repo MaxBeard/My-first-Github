6ecdb6c015c40d24420d75f287781faa9fb69cbd
MaxBeard <mbeard@sjs.org>
2019-11-19 14:24:39 -0600

    added for loop

--- HelloWorldMB/src/helloworldmb/HelloWorldMB.java
+++ HelloWorldMB/src/helloworldmb/HelloWorldMB.java
@@ -1,8 +1,11 @@
 public class HelloWorldMB {
 
     public static void main(String[] args) {
-        System.out.println("Hello WoRlD");
-        System.out.println("Hello part 2");
+        System.out.println("Hello world");
+        for(int i=1;i<11;i++){
+            System.out.println(i);
+        }
+               
     }
     
 }
