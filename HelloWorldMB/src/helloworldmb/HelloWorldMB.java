86b0c2528b6c8aef927aa545aed5b656cedda81c
MaxBeard <mbeard@sjs.org>
2019-11-22 12:00:29 -0600

    Update HelloWorldMB.java

--- HelloWorldMB/src/helloworldmb/HelloWorldMB.java
+++ HelloWorldMB/src/helloworldmb/HelloWorldMB.java
@@ -5,6 +5,10 @@ public class HelloWorldMB {
     public static void main(String[] args) {
 //        System.out.println("Hi");
         System.out.println( "rofl, hacked" );
+        System.out.println("Numbers up to 10");
+        for (int i=0;i<11;i++){
+            System.out.println(i);
+        }
     }
                
 }
